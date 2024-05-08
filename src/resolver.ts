63248a9c1284b8c2579c78253e2c4b3760eb78af
santoine@nerdery.com <santoine@nerdery.com>
2024-05-08 13:22:23 -0400

    update comments

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,19 +1,19 @@
 // resolver.ts
-
+import {userData1, userData2} from "./data";
 import {DataSource, User} from "./types";
 
 export function getUsers(): User[] {
-    // TODO: You need to merge data from both sources and return
+    // TODO: You need to merge userData1 / userData2 from both sources and return in structure defined within the schema
     return [];
 }
 
 export function getUserById(id: string | number, source: DataSource): User | null {
-    // TODO: You need to find user by ID in the appropriate data source
+    // TODO: You need to find user by ID in the appropriate data source. Note, id can be uuid or int id
     return null;
 }
 
 function normalizeUser(user: any): User {
-    // TODO: You need to normalize user object to match schema structure
+    // TODO: You need to normalize the user object to match the schema structure below
     return {
         id: '',
         firstName: '',
